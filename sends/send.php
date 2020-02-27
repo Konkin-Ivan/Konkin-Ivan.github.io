34225ec70c7dbc5f46e53311f85e14c9ea26cf74
Ivan <konkin.ivan@yandex.ru>
2020-02-27 23:04:26 +0300

    Add files via upload

---
+++ sends/send.php
@@ -0,0 +1,10 @@
+﻿<meta http-equiv='refresh' content='6; url=dez-perm59@yandex.ru'>
+  <meta charset="UTF-8" />
+<?
+if (isset ($_POST['phone'])) {
+  mail ("dez-perm59@yandex.ru",
+        "Новая заявка с сайта на обратный звонок".$_SERVER['HTTP_REFERER'],
+        "Телефон: ".$_POST['phone']);
+  echo "<center><big><em>Ваша заявка на обратный звонок принята, мы Вам перезвоним в ближайшее время!</em></big></center><a href='dez-perm59@yandex.ru'></a>"; 
+}
+?>
---
+++ sends/send_2.php
@@ -0,0 +1,10 @@
+﻿<meta http-equiv='refresh' content='6; url=http://beznasekomyh59.ru/'>
+  <meta charset="UTF-8" />
+<?
+if (isset ($_POST['phone'])) {
+  mail ("dez-perm59@yandex.ru",
+        "Новая заявка на бесплатную консультацию или заказ".$_SERVER['HTTP_REFERER'],
+        "Имя: ".$_POST['name']."\nТелефон: ".$_POST['phone']);
+  echo "<center><big><em>Ваш заявка успешно принята! Мы с Вами свяжемся в ближайшее время!</em></big></center><a href='http://beznasekomyh59.ru/'></a>"; 
+}
+?>
